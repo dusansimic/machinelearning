d5d9a2ea3fa5e2bc216addb18a80d3e8535175e9
Dušan Simić <dusan.simic1810@gmail.com>
2017-07-14 19:37:48 +0200

    izmene

--- MLProjekatAutotest.py
+++ MLProjekatAutotest.py
@@ -6,21 +6,21 @@ fl = []
 features = []
 labels = []
 arr2 = []
-inFile = open("C:/Users/NESA/Documents/petnica/seminar/test_noclass.csv", "r")
+inFile = open("test-noclass.csv", "r")
 next(inFile)
 for line in inFile:
-    N += 1
-    arr1 = line.split(',')
-    for i in range(0, len(arr1)):
-        arr2.append(float(arr1[i]))
+  N += 1
+  arr1 = line.split(',')
+  for i in range(0, len(arr1)):
+    arr2.append(float(arr1[i]))
     fl.append(arr2[:])
-    arr1 = []
-    arr2 = []
+  arr1 = []
+  arr2 = []
 print N
 np.random.shuffle(fl)
 for i in range(0, N):
-    features.append(fl[i][:-1])
-    labels.append(fl[i][-1])
+  features.append(fl[i][:-1])
+  labels.append(fl[i][-1])
     
 trainingFeatures, testFeatures, trainingLabels, testLabels = train_test_split(features, labels, test_size = 0.2)
 print "datasets generated"
@@ -33,11 +33,10 @@ pred = model.predict(testFeatures)
 hits = 0
 co = 0
 
-for i in range(len(testFeatures)):
-    	if co < 10: 
-		print "-", testLabels[i], "-", pred[i]
-		co += 1
-	if testLabels[i] == pred[i]: hits += 1
+with open('output.txt', 'w') as outputFile:
+  for i in range(len(testFeatures)):
+    outputFile.write(testLabels[i], '-', pred[i], '\n')
+    if testLabels[i] == pred[i]: hits += 1
 
 print round(1.0 * hits / len(testFeatures)*100,2),"%"
 inFile.close()
